eeffb0067b84aa9cb00a9b68d6bb4372ee740086
Ezequiel Colunga Acha <76527474+EzequielColungaAcha@users.noreply.github.com>
2026-02-01 21:07:32 -0300

    Enable maintenance mode with updated message

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const CONFIG = {
   currency: '$',
   // Maintenance mode configuration
   maintenance: {
-    enabled: false,
-    message: 'Estamos en mantenimiento. Volvemos pronto!',
+    enabled: true,
+    message: 'No tenemos stock. Nos reencontramos el domingo 8 de febrero en la Noche de Verano en la Plaza San Martín!',
   },
   // Operating hours configuration
   operatingHours: {
